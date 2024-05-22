0e044c24b094aa18052f1a1f5cad7f0793ca05c7
Margaret <magpat0783@gmail.com>
2024-05-22 20:06:45 +0200

    Added BrowserFactory code

--- src/test/java/features/Login.feature
+++ src/test/java/features/Login.feature
@@ -1,4 +1,27 @@
 Feature: Customer initial deposit
+  Scenario: Clear Account Balance status
+    Given the User is on the homepage "http://www.way2automation.com/angularjs-protractor/banking/#/login"
+    When the User Clicks the Customer Login Button
+    And the User Clicks Dropdown List
+    Then the User Selects <yourName>
+    And the User Clicks on Login Button
+    Then Welcome <yourName> !! message is displayed
+    And User Clicks on Account Dropdown List
+    And Selects Account number
+    Then User verifies account balance
+#  Scenario: Login as a Customer
+#    Given the User is on the homepage "http://www.way2automation.com/angularjs-protractor/banking/#/login"
+#    When the User Clicks the Customer Login Button
+#    And the User Clicks Dropdown List
+#    Then the User Selects <yourName>
+#    And the User Clicks on Login Button
+#
+#    And the User logs out
+#
+#  Scenario: Customer deposits money into their account and validates the deposit
+#    And User Clicks on Transactions Button
+#    Then
+Feature: Customer initial deposit
 
   Scenario: Clear Account Balance status
     Given the User is on the homepage "http://www.way2automation.com/angularjs-protractor/banking/#/login"
--- src/test/java/utils/BrowserFactory.java
+++ src/test/java/utils/BrowserFactory.java
@@ -1,5 +1,35 @@
 package utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class BrowserFactory {
+    public static WebDriver driver;
+
+    public static WebDriver startBrowser(String browserChoice, String url) {
+        if ("chrome".equalsIgnoreCase(browserChoice)) {
+            ChromeOptions options = new ChromeOptions();
+//            options.addArguments("--headless"); //Do the same with other browsers, insert this line
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver(options);//For the code to run headless you must intergrate it 1st, insert the "options" in ln19
+        } else if ("firefox".equalsIgnoreCase(browserChoice)) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if ("edge".equalsIgnoreCase(browserChoice)) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        } else {
+            WebDriverManager.safaridriver().setup();
+            driver = new SafariDriver();
+        }
+        driver.manage().window().maximize();
+        driver.get(url);
+        return driver;
+    }
 
 }
